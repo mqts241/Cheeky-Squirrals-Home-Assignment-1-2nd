62bd9e8debb792a4453acf1952a5a2acad6e81e0
mqts241 <mats.haertel@gmx.de>
2025-03-01 15:12:34 +0100

    gitignore

--- Test/NomeDelProgetto/obj/Debug/net9.0/NomeDelProgetto.AssemblyInfo.cs
+++ Test/NomeDelProgetto/obj/Debug/net9.0/NomeDelProgetto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NomeDelProgetto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97fa038079809985cf74d58986f3382aa613f450")]
 [assembly: System.Reflection.AssemblyProductAttribute("NomeDelProgetto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NomeDelProgetto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
